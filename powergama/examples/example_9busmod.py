151db758b3925208f0d8102ab9300914a0293294
Harald G Svendsen <harald.svendsen@sintef.no>
2014-03-05 16:24:14 +0100

    Added new 9-bus example (not complete yet)

---
+++ powergama/examples/example_9busmod.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Illustrative example based on the IEEE 9 bus system
+
+
+linearised optimal power flow does not seem to give a very good approximation
+"""
+
+import powergama
+import time
+
+datapath= "data/9busmod_"
+timerange=range(0,5)
+
+data = powergama.GridData()
+
+data.readGridData(nodes=datapath+"nodes.csv",
+                  ac_branches=datapath+"branches.csv",
+                  dc_branches=None,
+                  generators=datapath+"generators.csv",
+                  consumers=datapath+"consumers.csv")
+data.readProfileData(inflow=datapath+"profiles_inflow.csv",
+            demand=datapath+"profiles_demand.csv",
+            storagevalue_filling=datapath+"profiles_storval_filling.csv",
+            storagevalue_time=datapath+"profiles_storval_time.csv",
+            timerange=timerange, 
+            timedelta=1.0)
+
+lp = powergama.LpProblem(data)
+start_time = time.time()
+res = lp.solve()
+end_time = time.time()
+print end_time - start_time, "seconds"
+
