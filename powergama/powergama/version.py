8e7547dcd64c785d3378d1739a255963b146e906
Harald G Svendsen <harald.svendsen@sintef.no>
2014-12-04 11:32:32 +0100

    Changed to version 0.8.0 Small changes to make package installable via pip install

--- powergama/powergama/version.py
+++ powergama/powergama/version.py
@@ -3,5 +3,5 @@
 PowerGAMA version information
 '''
 
-__version__ = '0.7.0'
-__version_date__ = '2014-08-05'
+__version__ = '0.8.0'
+__version_date__ = '2014-12-04'
--- powergama/setup.py
+++ powergama/setup.py
@@ -5,11 +5,25 @@ exec(open('powergama/version.py').read())
 setup(name='powergama',
       version=__version__,
       description='PowerGAMA - Power Grid And Market Analysis tool',
+	  long_description = 'PowerGAMA is a Python-based lightweight simulation tool for high level analyses of renewable energy integration in large power systems. The simulation tool optimises the generation dispatch, i.e. the power output from all generators in the power system, based on marginal costs for each timestep over a given period, for example one year. It takes into account the variable power available for solar, hydro and wind power generators. It also takes into account the variability of demand. Moreover, it is flow-based meaning that the power flow in the AC grid is determined by physical power flow equations.',
       url='https://bitbucket.org/harald_g_svendsen/powergama',
       author='Harald G Svendsen',
       author_email='harald.svendsen@sintef.no',
       license='MIT License (http://opensource.org/licenses/MIT)',
       packages=['powergama'],
+      zip_safe = True,
+	  classifiers = [
+		'Development Status :: 3 - Alpha',
+		'Intended Audience :: Science/Research',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 2.7'
+	  ],
+	  keywords = 'power systems, grid integration, renewable energy',
+	  install_requires = [
+		'numpy',
+		'scipy',
+		'matplotlib',
+		'PuLP>=1.5.6'
+		],
 	  data_files = [('', ['licence.txt'])],
-      zip_safe = True
 	 )
