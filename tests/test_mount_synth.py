5681db1fc95a99f6ceb24f13ebe29591d9fb6b0c
Sigrid Passano Hellan <sigrid.hellan@sintef.no>
2024-02-02 13:54:06 +0100

    More tests for mount_synth example, and warning if pump capacity is postitive but efficiency not.

--- tests/test_mount_synth.py
+++ tests/test_mount_synth.py
@@ -9,33 +9,44 @@ datapath = Path(__file__).parent / "test_data/mount_synth"
 data_prefix = "mount_"
 
 
-gridmodel_shed = powergama.GridData()
-gridmodel_shed.readGridData(
-    nodes=datapath / (data_prefix + "nodes.csv"),
-    ac_branches=datapath / (data_prefix + "branches.csv"),
-    dc_branches=datapath / (data_prefix + "hvdc.csv"),
-    generators=datapath / (data_prefix + "generators.csv"),
-    consumers=datapath / (data_prefix + "consumers.csv"),
-)
-gridmodel_shed.readProfileData(filename=datapath / (data_prefix + "profiles.csv"), timerange=range(0, 3), timedelta=1.0)
+def load_gridmodel(name):
+    if name == "shed":
+        generators_file = "generators.csv"
+        storagevalue_filling_file = None
+        storagevalue_time_file = None
+    elif name == "battery":
+        generators_file = "generators_storage.csv"
+        storagevalue_filling_file = datapath / (data_prefix + "storval_filling.csv")
+        storagevalue_time_file = datapath / (data_prefix + "storval_time.csv")
+    elif name == "zero_eff":
+        generators_file = "generators_zero_eff_storage.csv"
+        storagevalue_filling_file = datapath / (data_prefix + "storval_filling.csv")
+        storagevalue_time_file = datapath / (data_prefix + "storval_time.csv")
 
-gridmodel_battery = powergama.GridData()
-gridmodel_battery.readGridData(
-    nodes=datapath / (data_prefix + "nodes.csv"),
-    ac_branches=datapath / (data_prefix + "branches.csv"),
-    dc_branches=datapath / (data_prefix + "hvdc.csv"),
-    generators=datapath / (data_prefix + "generators_storage.csv"),
-    consumers=datapath / (data_prefix + "consumers.csv"),
-)
-gridmodel_battery.readProfileData(
-    filename=datapath / (data_prefix + "profiles.csv"),
-    storagevalue_filling=datapath / (data_prefix + "storval_filling.csv"),
-    storagevalue_time=datapath / (data_prefix + "storval_time.csv"),
-    timerange=range(0, 3),
-    timedelta=1.0,
-)
+    gridmodel = powergama.GridData()
+    gridmodel.readGridData(
+        nodes=datapath / (data_prefix + "nodes.csv"),
+        ac_branches=datapath / (data_prefix + "branches.csv"),
+        dc_branches=datapath / (data_prefix + "hvdc.csv"),
+        generators=datapath / (data_prefix + generators_file),
+        consumers=datapath / (data_prefix + "consumers.csv"),
+    )
+    gridmodel.readProfileData(
+        filename=datapath / (data_prefix + "profiles.csv"),
+        storagevalue_filling=storagevalue_filling_file,
+        storagevalue_time=storagevalue_time_file,
+        timerange=range(0, 3),
+        timedelta=1.0,
+    )
+    return gridmodel
 
-gridmodels = {"shed": gridmodel_shed, "battery": gridmodel_battery}
+
+def test_warn_zero_eff_storage():
+    """Check that a warning is triggered if a generator is specified with
+    non-zero capacity but zero efficiency.
+    """
+    with pytest.warns(UserWarning, match="very low pump efficiency"):
+        load_gridmodel("zero_eff")
 
 
 def solve_problem(gridmodel, name):
@@ -49,18 +60,39 @@ def solve_problem(gridmodel, name):
 
 
 exp_load_shedding = {"shed": np.array([3, 0, 0]), "battery": np.zeros(3)}
-
+exp_system_cost = {"shed": 2.7, "battery": 3.6}
 exp_generation = {"shed": [[12, 10, 5], []], "battery": np.array([[15, 10, 5], [0, 0, 3]])}
+exp_DA = {(0, 0): -20, (0, 1): 20, (1, 0): -20, (1, 2): 20}
+exp_Bbus = {
+    (0, 2): -20.0,
+    (0, 1): -20.0,
+    (0, 0): 40.0,
+    (1, 1): 20.0,
+    (1, 0): -20.0,
+    (2, 2): 20.0,
+    (2, 0): -20.0,
+}
+
+
+def test_power_flow_matrices():
+    """Check that the power flow matrices are as expected."""
+    gridmodel = load_gridmodel("shed")
+    Bbus, DA = gridmodel.compute_power_flow_matrices(base_Z=1)
+    assert exp_DA == pytest.approx(dict(DA.todok())), "Unexpected DA matrix"
+    assert exp_Bbus == pytest.approx(dict(Bbus.todok())), "Unexpected Bbus matrix"
 
 
 @pytest.mark.parametrize("scenario_name", ["shed", "battery"])
 def test_shedding(scenario_name):
     """
-    Runs two versions of the scenario, one with and one without storage. Expect the former to require load shedding.
+    Runs two versions of the scenario, one with and one without storage. We expect the latter to require load shedding.
     """
-    res = solve_problem(gridmodels[scenario_name], scenario_name)
+    gridmodel = load_gridmodel(scenario_name)
+    res = solve_problem(gridmodel, scenario_name)
 
-    assert exp_load_shedding[scenario_name] == pytest.approx(res.getLoadsheddingPerNode())
+    assert exp_load_shedding[scenario_name] == pytest.approx(res.getLoadsheddingPerNode()), "Unexpected load shedding"
+    assert exp_system_cost[scenario_name] == pytest.approx(res.getSystemCost()["mount"]), "Unexpected system cost"
     for gen in range(len(exp_generation[scenario_name])):
-        print(scenario_name, gen)
-        assert exp_generation[scenario_name][gen] == pytest.approx(res.db.getResultGeneratorPower(gen, [0, 3]))
+        assert exp_generation[scenario_name][gen] == pytest.approx(
+            res.db.getResultGeneratorPower(gen, [0, 3])
+        ), "Unexpected generation"
