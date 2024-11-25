e448af5eabfdb18982fa1e4f227b3ffea3d54f75
Harald G Svendsen <harald.svendsen@sintef.no>
2024-11-25 16:59:51 +0100

    change tempdir

--- tests/test_fault_scenario.py
+++ tests/test_fault_scenario.py
@@ -3,7 +3,6 @@ A set of tests for fault scenario analysis.
 """
 import copy
 import pathlib
-import tempfile
 
 import pandas as pd
 import pyomo.environ as pyo
@@ -19,7 +18,7 @@ def test_fault_scenario_creation():
 
 
 @pytest.mark.skipif(not pyo.SolverFactory("glpk").available(), reason="Skipping test because GLPK is not available.")
-def test_faultsimulation(testcase_9bus_data, testcase_9bus_res):
+def test_faultsimulation(testcase_9bus_data, testcase_9bus_res, tmp_path):
     """9 bus test to check that fault scenario simulation works, and gives expected result"""
 
     gridmodel_base = testcase_9bus_data
@@ -37,60 +36,55 @@ def test_faultsimulation(testcase_9bus_data, testcase_9bus_res):
     lp_base = powergama.LpProblem(gridmodel_base, lossmethod=1)
 
     # temporary folder for files created during test:
-    # dirname = "tempdir"
-    # if True:
-    with tempfile.TemporaryDirectory() as dirname:
-        base_db_loc = pathlib.Path(dirname).absolute() / "base_powergama.sqlite3"
-        print("Base database file:", base_db_loc.absolute())
+    dirname = tmp_path
 
-        # 1. Run baseline simulation and save result to database
-        res_base = powergama.Results(gridmodel_base, base_db_loc, replace=True)
-        lp_base.solve(res_base, solver="glpk")
+    base_db_loc = pathlib.Path(dirname).absolute() / "base_powergama.sqlite3"
+    print("Base database file:", base_db_loc.absolute())
 
-        # 2. Create fault scenario files:
-        fault_spec = fs.FaultSpec(spec_generators=fault_spec_generators)
-        failure_dir = pathlib.Path(dirname).absolute() / "faults"
-        print("failure_dir", failure_dir)
-        fs.create_fault_scenarios(gridmodel_base, failure_dir, fault_spec=fault_spec, seed_list=scenario_seeds)
+    # 1. Run baseline simulation and save result to database
+    res_base = powergama.Results(gridmodel_base, base_db_loc, replace=True)
+    lp_base.solve(res_base, solver="glpk")
 
-        # 3 Run simulations with faults (and save results to sql-files)
-        print("Running simulations with faults...")
-        for scen in scenario_seeds:
-            fault_scenario_dir = failure_dir / f"fault_scenario_{scen}"
-            fault_scenario_dir.mkdir(exist_ok=True)
-            fault_scenario_file = failure_dir / f"fault_scenario_{scen}.txt"
-            fs.run_fault_simulation(
-                gridmodel_base,
-                full_profiles,
-                fault_scenario_file=fault_scenario_file,
-                failure_dir=fault_scenario_dir,
-                db_base=base_db_loc,
-                solver="glpk",
-            )
+    # 2. Create fault scenario files:
+    fault_spec = fs.FaultSpec(spec_generators=fault_spec_generators)
+    failure_dir = pathlib.Path(dirname).absolute() / "faults"
+    print("failure_dir", failure_dir)
+    fs.create_fault_scenarios(gridmodel_base, failure_dir, fault_spec=fault_spec, seed_list=scenario_seeds)
 
-        # 4 Inspect results
-        print("Inspecting results...")
-        res_nodes_base = fs.specify_storage.load_table_from_res(base_db_loc, "Res_Nodes")
-        # res_gens = fs.specify_storage.load_table_from_res(base_db_loc, "Res_Generators")
-        base_node = copy.deepcopy(res_nodes_base)
-        # combine results from all fault situations:
-        loadshedding_all = pd.DataFrame()
-        for scen in scenario_seeds:
-            print(f"fault scenario {scen}")
-            fault_scenario_dir = failure_dir / f"fault_scenario_{scen}"
-            for subfolder in pathlib.Path(fault_scenario_dir).glob("failure_*/"):
-                res_table = fs.specify_storage.load_table_from_res(
-                    subfolder / "failure_case_combined.sqlite3", "Res_Nodes"
-                )
-                if (res_table["loadshed"] > 0).any():
-                    # update result with data from this fault situation:
-                    mask = base_node["timestep"].isin(res_table["timestep"])
-                    base_node.loc[mask, :] = res_table.drop(columns="fault_start").set_index(base_node.loc[mask].index)
-            loadshedding_all[scen] = base_node["loadshed"]
+    # 3 Run simulations with faults (and save results to sql-files)
+    print("Running simulations with faults...")
+    for scen in scenario_seeds:
+        fault_scenario_dir = failure_dir / f"fault_scenario_{scen}"
+        fault_scenario_dir.mkdir(exist_ok=True)
+        fault_scenario_file = failure_dir / f"fault_scenario_{scen}.txt"
+        fs.run_fault_simulation(
+            gridmodel_base,
+            full_profiles,
+            fault_scenario_file=fault_scenario_file,
+            failure_dir=fault_scenario_dir,
+            db_base=base_db_loc,
+            solver="glpk",
+        )
 
-        loadshedding_sums = loadshedding_all.sum()
+    # 4 Inspect results
+    print("Inspecting results...")
+    res_nodes_base = fs.specify_storage.load_table_from_res(base_db_loc, "Res_Nodes")
+    base_node = copy.deepcopy(res_nodes_base)
+    # combine results from all fault situations:
+    loadshedding_all = pd.DataFrame()
+    for scen in scenario_seeds:
+        print(f"fault scenario {scen}")
+        fault_scenario_dir = failure_dir / f"fault_scenario_{scen}"
+        for subfolder in pathlib.Path(fault_scenario_dir).glob("failure_*/"):
+            res_table = fs.specify_storage.load_table_from_res(subfolder / "failure_case_combined.sqlite3", "Res_Nodes")
+            if (res_table["loadshed"] > 0).any():
+                # update result with data from this fault situation:
+                mask = base_node["timestep"].isin(res_table["timestep"])
+                base_node.loc[mask, :] = res_table.drop(columns="fault_start").set_index(base_node.loc[mask].index)
+        loadshedding_all[scen] = base_node["loadshed"]
 
-        assert loadshedding_sums[1] == pytest.approx(815.8665322112945)
+    loadshedding_sums = loadshedding_all.sum()
 
-        print("Done with temporary folder")
-    print("Done. Temporary files deleted.")
+    assert loadshedding_sums[1] == pytest.approx(815.8665322112945)
+
+    print("Done.")
